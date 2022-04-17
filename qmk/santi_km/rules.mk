8969d12d8c1b771f184b6965b262b09ddf4e497b
Santiago Toscanini <toscaninisantiago@gmail.com>
2022-04-17 11:01:04 -0300

    [Automatically]: Update dotfiles.

--- qmk/santi_km/rules.mk
+++ qmk/santi_km/rules.mk
@@ -1,4 +1,5 @@
-RGBLIGHT_ENABLE = no
+# Inherits from https://github.com/qmk/qmk_firmware/blob/master/keyboards/crkbd/rules.mk
+
 RGB_MATRIX_ENABLE = yes
 
 OLED_ENABLE  = yes
@@ -7,13 +8,8 @@ WPM_ENABLE = yes
 
 # Optimizations
 LTO_ENABLE = yes
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
 SPACE_CADET_ENABLE = no
 GRAVE_ESCAPE_ENABLE = no
 # ------------
 
-NKRO_ENABLE = yes
-MOUSEKEY_ENABLE    = yes # Mouse keys
 TAP_DANCE_ENABLE   = yes # Tap dance functionality
-EXTRAKEY_ENABLE    = yes # Audio and system control
