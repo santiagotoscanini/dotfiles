3a2171518a44aff8a2ea126111ce8a35b876a650
Santiago Toscanini <toscaninisantiago@gmail.com>
2022-05-03 00:19:27 -0300

    [Automatically]: Update dotfiles.

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -84,8 +84,39 @@ cmp.setup.cmdline(':', {
 local nvim_lsp = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
+nvim_lsp.dartls.setup{ capabilities = capabilities }
 nvim_lsp.tsserver.setup{ capabilities = capabilities }
 nvim_lsp.solc.setup{ capabilities = capabilities }
 nvim_lsp.gopls.setup{ capabilities = capabilities }
-nvim_lsp.pyright.setup{ capabilities = capabilities }
-nvim_lsp.dartls.setup{ capabilities = capabilities }
+
+-- TODO(santiagotoscanini): make buffers aware of current virtual env (pyenv, poetry, venv, conda, etc)
+-- local path = util.path
+-- local function get_python_path(workspace)
+--   -- Use activated virtualenv.
+--   if vim.env.VIRTUAL_ENV then
+--     return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+--   end
+--
+--   -- Find and use virtualenv in workspace directory.
+--   for _, pattern in ipairs({'*', '.*'}) do
+--     local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
+--     if match ~= '' then
+--         return path.join(path.dirname(match), 'bin', 'python')
+--     end
+--
+--     local match = vim.fn.glob(path.join(workspace, 'poetry.lock'))
+--     if match ~= '' then
+--         local venv = vim.fn.trim(vim.fn.system('poetry env info -p'))
+--         return path.join(venv, 'bin', 'python')
+--     end
+--   end
+--
+--   -- Fallback to system Python.
+--   return exepath('python3') or exepath('python') or 'python'
+-- end
+nvim_lsp.pyright.setup{
+    capabilities = capabilities,
+    -- before_init = function(_, config)
+    --     config.settings.python.pythonPath = get_python_path(config.root_dir)
+    -- end
+}
