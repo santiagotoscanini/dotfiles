efbc04f041fb1a3b882ff5c3f2e716d3e1494d2b
Santiago Toscanini <toscaninisantiago@gmail.com>
2022-06-20 11:50:55 -0300

    [Automatically]: Update dotfiles.

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -85,7 +85,7 @@ local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protoco
 
 nvim_lsp.dartls.setup({ capabilities = capabilities })
 nvim_lsp.tsserver.setup({ capabilities = capabilities })
-nvim_lsp.solc.setup({ capabilities = capabilities })
+-- nvim_lsp.solc.setup({ capabilities = capabilities })
 nvim_lsp.gopls.setup({ capabilities = capabilities })
 nvim_lsp.vimls.setup({})
 
