07a6796141948f12e1a759eaea56c548560ef6fc
Santiago Toscanini <toscaninisantiago@gmail.com>
2022-07-20 12:47:22 -0300

    [Automatically]: Update dotfiles.

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,10 +1,11 @@
-addToPath /opt/homebrew/bin                                     # Homebrew
-addToPath ~/.poetry/bin                                         # Poetry (Python)
-addToPath ~/.cargo/bin                                          # Cargo (Rust)
-addToPath /Library/Frameworks/Python.framework/Versions/3.9/bin # Python 3.9
+addToPath /opt/homebrew/bin                                         # Homebrew
+addToPath ~/.poetry/bin                                             # Poetry (Python)
+addToPath ~/.cargo/bin                                              # Cargo (Rust)
+addToPath /Library/Frameworks/Python.framework/Versions/3.9/bin     # Python 3.9
+addToPath ~/Library/Application\ Support/JetBrains/Toolbox/scripts  # JetBrains scripts
 
 # Android
-addToPath $ANDROID_HOME/cmdline-tools/cmdline-tools/bin         # sdkmanager (Android SDK)
-addToPath $ANDROID_HOME/platform-tools/                         # adb (Android Debug Bridge), fastboot (Android Fastboot)
-addToPath $ANDROID_HOME/emulator                                # Android emulator
-addToPath /opt/homebrew/opt/openjdk/bin                         # Java (OpenJDK)
+addToPath $ANDROID_HOME/cmdline-tools/cmdline-tools/bin             # sdkmanager (Android SDK)
+addToPath $ANDROID_HOME/platform-tools/                             # adb (Android Debug Bridge), fastboot (Android Fastboot)
+addToPath $ANDROID_HOME/emulator                                    # Android emulator
+addToPath /opt/homebrew/opt/openjdk/bin                             # Java (OpenJDK)
