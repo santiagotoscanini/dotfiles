ea80afa1e267e522eb76bdce9aa7d3dfbb1f188f
Santiago Toscanini <toscaninisantiago@gmail.com>
2022-09-23 19:02:34 -0300

    [Automatically]: Update dotfiles.

--- zsh/git-alias.zsh
+++ zsh/git-alias.zsh
@@ -14,7 +14,7 @@ commitDotfiles() {
 commitNotes() {
     pushd $NOTES_DIR
         git add .
-        git commit -m "[Automatically]: Update notes."
+        git commit -m "[Automatically]: Vault backup from $HOST_NAME (CLI)"
         git push
     popd
 }
