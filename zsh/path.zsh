1c5a3d62ad8625dbc077048fa5fbb19bbeb08f80
Santiago Toscanini <toscaninisantiago@gmail.com>
2022-10-31 21:49:41 -0300

    [Automatically]: Update dotfiles.

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -9,3 +9,8 @@ addToPath $ANDROID_HOME/cmdline-tools/cmdline-tools/bin             # sdkmanager
 addToPath $ANDROID_HOME/platform-tools/                             # adb (Android Debug Bridge), fastboot (Android Fastboot)
 addToPath $ANDROID_HOME/emulator                                    # Android emulator
 addToPath /opt/homebrew/opt/openjdk/bin                             # Java (OpenJDK)
+
+# Google Cloud CLI
+. '/usr/local/bin/google-cloud-sdk/path.zsh.inc'
+. '/usr/local/bin/google-cloud-sdk/completion.zsh.inc'
+
