7287ea3ba231faf15357a1cdc8f1f3a8ba4d0aae
Santiago Toscanini <toscaninisantiago@gmail.com>
2022-12-22 00:52:33 -0300

    [Automatically]: Update dotfiles.

--- zsh/change-theme.zsh
+++ zsh/change-theme.zsh
@@ -1,8 +1,8 @@
 function _change_alacritty_theme() {
-    sed -i '' "s/^colors:\ \*.*$/colors:\ \*$1/" $(realpath $XDG_CONFIG_HOME/alacritty/color.yml)
+    sed -i '' "s/^colors:\ \*.*$/colors:\ \*$1/" "$(realpath "$XDG_CONFIG_HOME"/alacritty/color.yml)"
 }
 
-function autoSwitchAlacrittyTheme() {
+function _autoSwitchAlacrittyTheme() {
     if defaults read -g AppleInterfaceStyle > /dev/null 2>&1 = 'dark' ; then
         _change_alacritty_theme tokyonight_night
     else
@@ -10,13 +10,13 @@ function autoSwitchAlacrittyTheme() {
     fi
 }
 
-function autoSwitchVimTheme() {
+function _autoSwitchVimTheme() {
     for pid in $(pgrep vim); do
-        kill -SIGUSR1 $pid
+        kill -SIGUSR1 "$pid"
     done
 }
 
-function autoSwitchDesktopLights() {
+function _autoSwitchDesktopLights() {
     pushd "$DOTFILES_DIR"/smart-home-automations || exit
     if defaults read -g AppleInterfaceStyle > /dev/null 2>&1 = 'dark' ; then
         poetry run python3 main.py on
@@ -27,16 +27,11 @@ function autoSwitchDesktopLights() {
 }
 
 # TODO(santiagotoscanini): add more tmux themes
-#function autoSwitchTmuxTheme() {
+#function _autoSwitchTmuxTheme() {
 #}
 
 function autoSwitchEverything() {
-    autoSwitchAlacrittyTheme
-    autoSwitchVimTheme
-    autoSwitchDesktopLights
+    _autoSwitchAlacrittyTheme
+    _autoSwitchVimTheme
+    _autoSwitchDesktopLights
 }
-
-# This is to check the color settings when loading the file from alacritty
-# if [ "$TERM_PROGRAM" != "vscode" ] && [ "$TERMINAL_EMULATOR" != "JetBrains-JediTerm" ] && [ -z "$INTELLIJ_ENVIRONMENT_READER" ]; then
-#     autoSwitchAlacrittyTheme
-# fi
--- zsh/git-alias.zsh
+++ zsh/git-alias.zsh
@@ -1,20 +1,27 @@
 commitDotfiles() {
-    pushd $DOTFILES_DIR
-        pushd work-dotfiles
+    pushd "$DOTFILES_DIR" || exit
+        pushd work-dotfiles || exit
             git add .
             git commit -m "[Automatically]: Update work-dotfiles."
             git push
-        popd
+        popd || exit
+
+        pushd smart-home-automations || exit
+            git add .
+            git commit -m "[Automatically]: Update smart-home-automations."
+            git push
+        popd || exit
+
         git add .
         git commit -m "[Automatically]: Update dotfiles."
         git push origin main
-    popd
+    popd || exit
 }
 
 commitNotes() {
-    pushd $NOTES_DIR
+    pushd "$NOTES_DIR" || exit
         git add .
         git commit -m "[Automatically]: Vault backup from $HOSTNAME (CLI)"
         git push
-    popd
+    popd || exit
 }
